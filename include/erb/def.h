2e9d6fcbb6f97147e4bc93994259edbc40868d14
rdi <raphael.dinge@ohmforce.com>
2021-04-07 21:42:20 +0200

    Add disable warnings macro for daisy headers include

--- include/erb/def.h
+++ include/erb/def.h
@@ -24,5 +24,34 @@
 #endif
 
 
+/*\\\ WARNINGS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+#if defined (__clang__)
+   #define erb_DISABLE_WARNINGS_DAISY \
+      _Pragma ("clang diagnostic push")
+
+   #define erb_RESTORE_WARNINGS \
+      _Pragma ("clang diagnostic pop")
+
+#elif defined (__GNUC__)
+   #define erb_DISABLE_WARNINGS_DAISY \
+      _Pragma ("GCC diagnostic push") \
+      _Pragma ("GCC diagnostic ignored \"-Wpedantic\"") \
+      _Pragma ("GCC diagnostic ignored \"-Wignored-qualifiers\"") \
+      _Pragma ("GCC diagnostic ignored \"-Wunused-parameter\"") \
+
+   #define erb_RESTORE_WARNINGS \
+      _Pragma ("GCC diagnostic pop")
+
+#elif defined (_MSC_VER)
+   #define erb_DISABLE_WARNINGS_DAISY \
+      __pragma (warning (push))
+
+   #define erb_RESTORE_WARNINGS \
+      __pragma (warning (pop))
+
+#endif
+
+
 
 /*\\\ EOF \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
