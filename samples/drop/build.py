1d689359cc34dca57bfe5405191a9f1829e19381
rdi <raphael.dinge@ohmforce.com>
2021-04-09 19:00:23 +0200

    Add erb target argument to build script

--- samples/drop/build.py
+++ samples/drop/build.py
@@ -9,6 +9,7 @@
 ##### IMPORT #################################################################
 
 from __future__ import print_function
+import argparse
 import os
 import subprocess
 import sys
@@ -29,12 +30,37 @@ if sys.version_info < (2, 7):
 
 
 
+"""
+==============================================================================
+Name : parse_args
+==============================================================================
+"""
+
+def parse_args ():
+   arg_parser = argparse.ArgumentParser ()
+
+   arg_parser.add_argument(
+      '--erb-target',
+      default = 'daisy',
+      choices = ['daisy', 'vcvrack'],
+      help = 'The erb target to use. Defaults to daisy'
+   )
+
+   return arg_parser.parse_args (sys.argv[1:])
+
+
+
 ##############################################################################
 
 if __name__ == '__main__':
    try:
-      erbb.build_target ('drop', 'drop-daisy', PATH_THIS)
-      erbb.objcopy ('drop-daisy', PATH_THIS)
+      args = parse_args ()
+      if args.erb_target == 'daisy':
+         erbb.build_target ('drop', 'drop-daisy', PATH_THIS)
+         erbb.objcopy ('drop-daisy', PATH_THIS)
+
+      elif args.erb_target == 'vcvrack':
+         erbb.build_native_target ('drop', 'drop-vcvrack', PATH_THIS)
 
    except subprocess.CalledProcessError as error:
       print ('Build command exited with %d' % error.returncode, file = sys.stderr)
