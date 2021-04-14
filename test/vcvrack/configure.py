02badd837d6e3dd0aebf0aba32be98123dc763ba
rdi <raphael.dinge@ohmforce.com>
2021-04-14 10:07:30 +0200

    Add integration tests for vcvrack generators

---
+++ test/vcvrack/configure.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+#
+#     configure.py
+#     Copyright (c) 2020 Raphael Dinge
+#
+#Tab=3########################################################################
+
+
+##### IMPORT #################################################################
+
+from __future__ import print_function
+import os
+import subprocess
+import sys
+
+PATH_THIS = os.path.abspath (os.path.dirname (__file__))
+PATH_ROOT = os.path.abspath (os.path.dirname (os.path.dirname (PATH_THIS)))
+
+sys.path.insert (0, os.path.join (PATH_ROOT, 'build-system'))
+import erbb
+
+
+
+##############################################################################
+
+if sys.version_info < (3, 7):
+   print ('This script requires python 3.7 or greater.', file = sys.stderr)
+   sys.exit (1)
+
+
+
+##############################################################################
+
+if __name__ == '__main__':
+   try:
+      ast = erbb.parse_ui ('VcvRackUi.py', PATH_THIS)
+
+      erbb.configure ('vcvrack', PATH_THIS)
+      erbb.generate_vcvrack_panel ('vcvrack', PATH_THIS, ast)
+      erbb.generate_vcvrack_manifest ('vcvrack', PATH_THIS, ast)
+      erbb.generate_vcvrack_code ('vcvrack', PATH_THIS, ast)
+      erbb.generate_vcvrack_deploy ('vcvrack', PATH_THIS, ast)
+
+   except subprocess.CalledProcessError as error:
+      print ('Configure command exited with %d' % error.returncode, file = sys.stderr)
+      sys.exit (1)
