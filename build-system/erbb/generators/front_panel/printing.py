9d074c25b54e00fbe3cde5cf5e6732755723f495
rdi <raphael.dinge@ohmforce.com>
2021-04-15 18:04:06 +0200

    Add front panel print generator

---
+++ build-system/erbb/generators/front_panel/printing.py
@@ -0,0 +1,42 @@
+##############################################################################
+#
+#     printing.py
+#     Copyright (c) 2020 Raphael DINGE
+#
+#Tab=3########################################################################
+
+
+
+import cairo
+import math
+import os
+
+from ..detail.panel import Panel as detailPanel
+
+
+
+MM_TO_PT = 72.0 / 25.4
+MODULE_HEIGHT = 128.5#mm
+
+
+
+class Printing:
+
+   #--------------------------------------------------------------------------
+
+   def generate (self, name, path, root):
+      for module in root.modules:
+         self.generate_module (name, path, module)
+
+
+   #--------------------------------------------------------------------------
+
+   def generate_module (self, name, path, module):
+      path_artifacts = os.path.join (path, 'artifacts')
+      path_pdf = os.path.join (path_artifacts, '%s.pdf' % name)
+
+      with cairo.PDFSurface (path_pdf, module.width * MM_TO_PT, MODULE_HEIGHT * MM_TO_PT) as surface:
+         context = cairo.Context (surface)
+
+         panel = detailPanel ()
+         panel.generate_module (context, module, render_back=False)
