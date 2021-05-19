924b526564aaa049b73064e9457c22f42c3dbefc
rdi <raphael.dinge@ohmforce.com>
2021-05-19 22:17:49 +0200

    WIP

---
+++ boards/daisy.field/definition.py
@@ -0,0 +1,44 @@
+##############################################################################
+#
+#     definition.py
+#     Copyright (c) 2020 Raphael DINGE
+#
+#Tab=3########################################################################
+
+
+
+{
+   'pins': {
+      'CI1': { 'accept': ['CvIn'] },
+      'CI2': { 'accept': ['CvIn'] },
+      'CI3': { 'accept': ['CvIn'] },
+      'CI4': { 'accept': ['CvIn'] },
+
+      'CO1': { 'accept': ['CvOut'] },
+      'CO2': { 'accept': ['CvOut'] },
+
+      'GI': { 'accept': ['GateIn'] },
+      'GO': { 'accept': ['GateOut'] },
+
+      'L1': { 'accept': ['Led'] },
+      'L2': { 'accept': ['Led'] },
+      'L3': { 'accept': ['Led'] },
+      'L4': { 'accept': ['Led'] },
+      'L5': { 'accept': ['Led'] },
+      'L6': { 'accept': ['Led'] },
+      'L7': { 'accept': ['Led'] },
+      'L8': { 'accept': ['Led'] },
+
+      'P1': { 'accept': ['Pot'] },
+      'P2': { 'accept': ['Pot'] },
+      'P3': { 'accept': ['Pot'] },
+      'P4': { 'accept': ['Pot'] },
+      'P5': { 'accept': ['Pot'] },
+      'P6': { 'accept': ['Pot'] },
+      'P7': { 'accept': ['Pot'] },
+      'P8': { 'accept': ['Pot'] },
+
+      'B1': { 'accept': ['Button'] },
+      'B2': { 'accept': ['Button'] },
+   }
+}
---
+++ boards/daisy.seed/definition.py
@@ -0,0 +1,230 @@
+##############################################################################
+#
+#     definition.py
+#     Copyright (c) 2020 Raphael DINGE
+#
+#Tab=3########################################################################
+
+
+
+{
+   'pins': {
+      'Pin0': {
+         'physical': 1,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin1': {
+         'physical': 2,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin2': {
+         'physical': 3,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin3': {
+         'physical': 4,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin4': {
+         'physical': 5,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin5': {
+         'physical': 6,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin6': {
+         'physical': 7,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin7': {
+         'physical': 8,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin8': {
+         'physical': 9,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin9': {
+         'physical': 10,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin1': {
+         'physical': 2,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin10': {
+         'physical': 11,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin12': {
+         'physical': 13,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin13': {
+         'physical': 14,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin14': {
+         'physical': 15,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin15': {
+         'physical': 22,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin16': {
+         'physical': 23,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin17': {
+         'physical': 24,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin18': {
+         'physical': 25,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin19': {
+         'physical': 26,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin20': {
+         'physical': 27,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin21': {
+         'physical': 28,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin22': {
+         'physical': 29,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin23': {
+         'physical': 30,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin24': {
+         'physical': 31,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin25': {
+         'physical': 32,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin26': {
+         'physical': 33,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin27': {
+         'physical': 34,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin28': {
+         'physical': 35,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin29': {
+         'physical': 36,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin30': {
+         'physical': 37,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+
+
+      'AudioInPinLeft': {
+         'physical': 16,
+         'accept': ['AudioIn'],
+      },
+      'AudioInPin0': {
+         'physical': 16,
+         'accept': ['AudioIn'],
+      },
+      'AudioInPinRight': {
+         'physical': 17,
+         'accept': ['AudioIn'],
+      },
+      'AudioInPin1': {
+         'physical': 17,
+         'accept': ['AudioIn'],
+      },
+      'AudioOutPinLeft': {
+         'physical': 18,
+         'accept': ['AudioOut'],
+      },
+      'AudioOutPin0': {
+         'physical': 18,
+         'accept': ['AudioOut'],
+      },
+      'AudioOutPinRight': {
+         'physical': 19,
+         'accept': ['AudioOut'],
+      },
+      'AudioOutPin1': {
+         'physical': 19,
+         'accept': ['AudioOut'],
+      },
+
+      'AdcPin0': {
+         'physical': 15,
+         'accept': ['CvIn', 'Pot'],
+      },
+      'AdcPin1': {
+         'physical': 16,
+         'accept': ['CvIn', 'Pot'],
+      },
+      'AdcPin2': {
+         'physical': 17,
+         'accept': ['CvIn', 'Pot'],
+      },
+      'AdcPin3': {
+         'physical': 18,
+         'accept': ['CvIn', 'Pot'],
+      },
+      'AdcPin4': {
+         'physical': 19,
+         'accept': ['CvIn', 'Pot'],
+      },
+      'AdcPin5': {
+         'physical': 20,
+         'accept': ['CvIn', 'Pot'],
+      },
+      'AdcPin6': {
+         'physical': 21,
+         'accept': ['CvIn', 'Pot'],
+      },
+      'AdcPin7': {
+         'physical': 22,
+         'accept': ['CvIn', 'Pot'],
+      },
+      'AdcPin8': {
+         'physical': 23,
+         'accept': ['CvIn', 'Pot'],
+      },
+      'AdcPin9': {
+         'physical': 24,
+         'accept': ['CvIn', 'Pot'],
+      },
+      'AdcPin10': {
+         'physical': 25,
+         'accept': ['CvIn', 'Pot'],
+      },
+      'AdcPin11': {
+         'physical': 28,
+         'accept': ['CvIn', 'Pot'],
+      },
+
+      'DacPin1': { # not starting at zero because official pin out starts at 1
+         'physical': 23,
+         'accept': ['CvOut'],
+      },
+
+      'DacPin2': {
+         'physical': 22,
+         'accept': ['CvOut'],
+      },
+   }
+}
