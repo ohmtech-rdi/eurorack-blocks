3405e44f76ececbdbcb792ec2701ab64cd0d63a6
rdi <raphael.dinge@ohmforce.com>
2021-05-20 21:33:47 +0200

    Add daisy seed board definition

---
+++ boards/daisy_seed/definition.py
@@ -0,0 +1,247 @@
+##############################################################################
+#
+#     definition.py
+#     Copyright (c) 2020 Raphael DINGE
+#
+#Tab=3########################################################################
+
+
+
+{
+   'pins': {
+      'Pin0': {
+         'physical': 1,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin1': {
+         'physical': 2,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin2': {
+         'physical': 3,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin3': {
+         'physical': 4,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin4': {
+         'physical': 5,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin5': {
+         'physical': 6,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin6': {
+         'physical': 7,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin7': {
+         'physical': 8,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin8': {
+         'physical': 9,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin9': {
+         'physical': 10,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin10': {
+         'physical': 11,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin11': {
+         'physical': 12,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin12': {
+         'physical': 13,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin13': {
+         'physical': 14,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin14': {
+         'physical': 15,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin15': {
+         'physical': 22,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin16': {
+         'physical': 23,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin17': {
+         'physical': 24,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin18': {
+         'physical': 25,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin19': {
+         'physical': 26,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin20': {
+         'physical': 27,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin21': {
+         'physical': 28,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin22': {
+         'physical': 29,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin23': {
+         'physical': 30,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin24': {
+         'physical': 31,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin25': {
+         'physical': 32,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin26': {
+         'physical': 33,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin27': {
+         'physical': 34,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin28': {
+         'physical': 35,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin29': {
+         'physical': 36,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'Pin30': {
+         'physical': 37,
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+
+
+      'AudioInDaisyPinLeft': {
+         'physical': 16,
+         'accept': ['AudioInDaisy'],
+      },
+      'AudioInDaisyPin0': {
+         'physical': 16,
+         'accept': ['AudioInDaisy'],
+      },
+      'AudioInDaisyPinRight': {
+         'physical': 17,
+         'accept': ['AudioInDaisy'],
+      },
+      'AudioInDaisyPin1': {
+         'physical': 17,
+         'accept': ['AudioInDaisy'],
+      },
+      'AudioOutDaisyPinLeft': {
+         'physical': 18,
+         'accept': ['AudioOutDaisy'],
+      },
+      'AudioOutDaisyPin0': {
+         'physical': 18,
+         'accept': ['AudioOutDaisy'],
+      },
+      'AudioOutDaisyPinRight': {
+         'physical': 19,
+         'accept': ['AudioOutDaisy'],
+      },
+      'AudioOutDaisyPin1': {
+         'physical': 19,
+         'accept': ['AudioOutDaisy'],
+      },
+
+      'AdcPin0': {
+         'physical': 22,
+         'accept': ['CvIn', 'Pot', 'Trim'],
+      },
+      'AdcPin1': {
+         'physical': 23,
+         'accept': ['CvIn', 'Pot', 'Trim'],
+      },
+      'AdcPin2': {
+         'physical': 24,
+         'accept': ['CvIn', 'Pot', 'Trim'],
+      },
+      'AdcPin3': {
+         'physical': 25,
+         'accept': ['CvIn', 'Pot', 'Trim'],
+      },
+      'AdcPin4': {
+         'physical': 26,
+         'accept': ['CvIn', 'Pot', 'Trim'],
+      },
+      'AdcPin5': {
+         'physical': 27,
+         'accept': ['CvIn', 'Pot', 'Trim'],
+      },
+      'AdcPin6': {
+         'physical': 28,
+         'accept': ['CvIn', 'Pot', 'Trim'],
+      },
+      'AdcPin7': {
+         'physical': 29,
+         'accept': ['CvIn', 'Pot', 'Trim'],
+      },
+      'AdcPin8': {
+         'physical': 30,
+         'accept': ['CvIn', 'Pot', 'Trim'],
+      },
+      'AdcPin9': {
+         'physical': 31,
+         'accept': ['CvIn', 'Pot', 'Trim'],
+      },
+      'AdcPin10': {
+         'physical': 32,
+         'accept': ['CvIn', 'Pot', 'Trim'],
+      },
+      'AdcPin11': {
+         'physical': 35,
+         'accept': ['CvIn', 'Pot', 'Trim'],
+      },
+
+      'DacPin1': { # not starting at zero because official pin out starts at 1
+         'physical': 30,
+         'accept': ['CvOut'],
+      },
+
+      'DacPin2': {
+         'physical': 29,
+         'accept': ['CvOut'],
+      },
+
+      # virtual pins for simulator integration tests
+      'PinNC': {
+         'accept': ['Button', 'GateIn', 'GateOut', 'Led', 'LedBi', 'LedRgb', 'Switch'],
+      },
+      'AudioInDaisyPinNC': {
+         'accept': ['AudioInDaisy'],
+      },
+      'AudioOutDaisyPinNC': {
+         'accept': ['AudioOutDaisy'],
+      },
+      'AdcPinNC': {
+         'accept': ['CvIn', 'Pot', 'Trim'],
+      },
+      'DacPinNC': {
+         'accept': ['CvOut'],
+      },
+   }
+}
