9bdd64ef49c4886c51021b49757cab2183349f26
rdi <raphael.dinge@ohmforce.com>
2022-01-09 14:04:09 +0100

    Update tests

--- test/field/configure.py
+++ test/field/configure.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
    try:
       ast_erbb = erbb.parse (os.path.join (PATH_THIS, 'Field.erbb'))
       erbb.generate_gyp (PATH_THIS, ast_erbb)
-      erbb.configure ('field', PATH_THIS)
+      erbb.configure (PATH_THIS, ast_erbb)
       erbb.cleanup (PATH_THIS)
 
       ast_erbui = erbui.parse (os.path.join (PATH_THIS, 'Field.erbui'))
