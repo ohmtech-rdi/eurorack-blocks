6a6e8e6903fadf8b752ddeebf4dac6958f72931b
rdi <raphael.dinge@ohmforce.com>
2022-01-11 09:35:58 +0100

    Update tests

--- test/data/Kivu12.cpp
+++ test/data/Kivu12.cpp
@@ -26,8 +26,8 @@ void  Kivu12::process ()
 #if 0
    for (size_t i = 0 ; i < erb_BUFFER_SIZE ; ++i)
    {
-      auto val = data.sample_mono.channels [0].samples [pos];
-      pos = (pos + 1) % data.sample_mono.channels [0].samples.size ();
+      auto val = data.sample_mono.frames [pos].channels [0];
+      pos = (pos + 1) % data.sample_mono.frames.size ();
 
       ui.audio_out1 [i] = val;
       ui.audio_out2 [i] = val;
@@ -38,7 +38,7 @@ void  Kivu12::process ()
    {
       ui.audio_out1 [i] = data.sample_stereo.frames [pos].channels [0];
       ui.audio_out2 [i] = data.sample_stereo.frames [pos].channels [1];
-      pos = (pos + 1) % data.sample_mono.frames.size ();
+      pos = (pos + 1) % data.sample_stereo.frames.size ();
    }
 #endif
 }
