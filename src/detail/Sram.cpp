bcace43cddf858456030d1e3e4991d2f1e6210d4
rdi <raphael.dinge@ohmforce.com>
2022-01-19 12:26:07 +0100

    Add sram nullptr on error allocation method

--- src/detail/Sram.cpp
+++ src/detail/Sram.cpp
@@ -51,6 +51,20 @@ Sram::Sram ()
 
 
 
+
+/*
+==============================================================================
+Name : allocate_bytes_nullptr_on_error
+==============================================================================
+*/
+
+void *   Sram::allocate_bytes_nullptr_on_error (size_t size)
+{
+   return use_instance ().allocate_raw_nullptr_on_error (1, size);
+}
+
+
+
 /*
 ==============================================================================
 Name : use_instance
@@ -86,6 +100,32 @@ Name : allocate_raw
 void *   Sram::allocate_raw (size_t alignment, size_t size)
 {
    const size_t pos = _memory_pool.allocate (alignment, size);
+
+#if defined (erb_TARGET_DAISY)
+   return &_sram_memory_pool_storage [pos];
+
+#else
+   ((void)(pos)); // ignore pos
+
+   // Not all c++17 cstdlib do have a 'std::aligned_alloc' implementation.
+   // Use 'std::malloc' as optimising alignment is not needed for the simulator.
+   return std::malloc (size);
+#endif
+}
+
+
+
+/*
+==============================================================================
+Name : allocate_raw_nullptr_on_error
+==============================================================================
+*/
+
+void *   Sram::allocate_raw_nullptr_on_error (size_t alignment, size_t size)
+{
+   const size_t pos = _memory_pool.allocate_npos_on_error (alignment, size);
+
+   if (pos == size_t (-1)) return nullptr;
 
 #if defined (erb_TARGET_DAISY)
    return &_sram_memory_pool_storage [pos];
