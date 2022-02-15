acfd874a68ec5b57f709f56807b49d0566bbd680
rdi <raphael.dinge@ohmforce.com>
2022-02-15 15:29:06 +0100

    Add support for zsh in init script

--- build-system/init.sh
+++ build-system/init.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-BUILD_SYSTEM_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+BUILD_SYSTEM_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-${(%):-%x}}" )" &> /dev/null && pwd )
 export PATH=$PATH:$BUILD_SYSTEM_DIR/scripts
