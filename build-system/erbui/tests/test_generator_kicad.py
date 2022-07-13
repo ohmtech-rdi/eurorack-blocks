b755067c9a2b80e5339e1a2719d2c2941d994b95
Raphael Dingé <raphael.dinge@ohmforce.com>
2022-07-13 21:06:47 +0200

    fixup! Add tests for kicad module

--- build-system/erbui/tests/test_generator_kicad.py
+++ build-system/erbui/tests/test_generator_kicad.py
@@ -11,7 +11,7 @@ import os
 import unittest
 import traceback
 
-from ..generators.kicad import sch
+from ..generators.kicad import sch, pcb
 
 
 PATH_THIS = os.path.abspath (os.path.dirname (__file__))
@@ -26,21 +26,34 @@ class TestGeneratorKicad (unittest.TestCase):
       if not os.path.exists (PATH_ARTIFACTS):
          os.makedirs (PATH_ARTIFACTS)
 
-   def test_000 (self):
+   def test_sch_000 (self):
       root = sch.Root.read (os.path.join (PATH_THIS, 'test_generator_kicad_asset_001.kicad_sch'))
       root.write (os.path.join (PATH_ARTIFACTS, 'test_generator_kicad_asset_001.kicad_sch'))
 
-   def test_001 (self):
+   def test_sch_001 (self):
       root = sch.Root.read (os.path.join (PATH_FRONT_PCB, 'alpha.9mm.manual', 'alpha.9mm.manual.kicad_sch'))
       root.write (os.path.join (PATH_ARTIFACTS, 'alpha.9mm.manual.kicad_sch'))
 
-   def test_002 (self):
+   def test_sch_002 (self):
       root = sch.Root.read (os.path.join (PATH_FRONT_PCB, 'alpha.9mm.wire', 'alpha.9mm.wire.kicad_sch'))
       root.write (os.path.join (PATH_ARTIFACTS, 'alpha.9mm.wire.kicad_sch'))
 
-   def test_003 (self):
+   def test_sch_003 (self):
       root = sch.Root.read (os.path.join (PATH_FRONT_PCB, 'rogan.2ps', 'rogan.2ps.kicad_sch'))
       root.write (os.path.join (PATH_ARTIFACTS, 'rogan.2ps.kicad_sch'))
+
+   def test_pcb_001 (self):
+      root = pcb.Root.read (os.path.join (PATH_FRONT_PCB, 'alpha.9mm.manual', 'alpha.9mm.manual.kicad_pcb'))
+      root.write (os.path.join (PATH_ARTIFACTS, 'alpha.9mm.manual.kicad_pcb'))
+
+   def test_pcb_002 (self):
+      root = pcb.Root.read (os.path.join (PATH_FRONT_PCB, 'alpha.9mm.wire', 'alpha.9mm.wire.kicad_pcb'))
+      root.write (os.path.join (PATH_ARTIFACTS, 'alpha.9mm.wire.kicad_pcb'))
+
+   def test_pcb_003 (self):
+      root = pcb.Root.read (os.path.join (PATH_FRONT_PCB, 'rogan.2ps', 'rogan.2ps.kicad_pcb'))
+      root.write (os.path.join (PATH_ARTIFACTS, 'rogan.2ps.kicad_pcb'))
+
 
 
 if __name__ == '__main__':
