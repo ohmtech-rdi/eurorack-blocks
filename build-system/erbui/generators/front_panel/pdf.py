e5d87631a684c5c7a68f14219fcd3e7d2e221d84
rdi <raphael.dinge@ohmforce.com>
2023-05-19 17:06:28 +0200

    Always add lib to path for find_library to find our dll

--- build-system/erbui/generators/front_panel/pdf.py
+++ build-system/erbui/generators/front_panel/pdf.py
@@ -18,10 +18,10 @@ PATH_THIS = os.path.abspath (os.path.dirname (__file__))
 PATH_BUILD_SYSTEM = os.path.abspath (os.path.dirname (os.path.dirname (os.path.dirname (PATH_THIS))))
 
 if platform.system () == 'Windows':
+   os.environ ['PATH'] += ';%s' % (os.path.join (PATH_BUILD_SYSTEM, 'lib'))
    if sys.version_info >= (3, 8):
       os.add_dll_directory (os.path.join (PATH_BUILD_SYSTEM, 'lib'))
-   else:
-      os.environ ['PATH'] += ';%s' % (os.path.join (PATH_BUILD_SYSTEM, 'lib'))
+
 import cairocffi
 
 
