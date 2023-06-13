04ea840cd6d34da232173ddccc923f87603ea0ce
Ricardo Matias <ricardo.matias0@gmail.com>
2023-06-13 12:50:29 +0200

    Expose the build-system folder in fish shell

--- build-system/init.fish
+++ build-system/init.fish
@@ -1,5 +1,11 @@
 #!/usr/bin/env fish
 
+set PREVIOUS_DIR (pwd)
+set BUILD_SYSTEM_DIR (cd (dirname (status -f)); and pwd)
+cd $PREVIOUS_DIR
+
+set -x -g PATH $PATH $BUILD_SYSTEM_DIR/scripts
+
 function __fish__eurorack-blocks_build-system_scripts_erbb_complete
    set -x ERBB_COMPLETION_FISH 1
    set -x COMP_LINE (commandline -p)
