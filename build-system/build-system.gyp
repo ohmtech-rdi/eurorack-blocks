150f09bb4d4263b9cd84c15dbd750aae7033b17f
rdi <raphael.dinge@ohmforce.com>
2023-06-18 15:48:55 +0200

    Add xcscheme to preconfigure debugging support

--- build-system/build-system.gyp
+++ build-system/build-system.gyp
@@ -84,6 +84,7 @@
             'erbb/generators/simulator/Makefile_template',
             'erbb/generators/simulator/make.py',
             'erbb/generators/simulator/xcode.py',
+            'erbb/generators/simulator/xcscheme_template',
 
             # generators/vcvrack
             'erbb/generators/vcvrack/project_template.gyp',
--- build-system/erbb/generators/simulator/xcode.py
+++ build-system/erbb/generators/simulator/xcode.py
@@ -10,6 +10,7 @@
 import fileinput
 import os
 import platform
+import random
 import sys
 
 PATH_THIS = os.path.abspath (os.path.dirname (__file__))
@@ -51,3 +52,30 @@ class Xcode:
 
          if 'BuildIndependentTargetsInParallel' in line:
             print ('\t\t\t\tLastUpgradeCheck = 2000;')
+
+      path_template = os.path.join (PATH_THIS, 'xcscheme_template')
+      path_xcschemes = os.path.join (project_path, 'xcshareddata', 'xcschemes')
+      path_xcscheme = os.path.join (path_xcschemes, '%s.xcscheme' % module.name)
+
+      if not os.path.exists (path_xcschemes):
+         os.makedirs (path_xcschemes)
+
+      with open (path_template, 'r', encoding='utf-8') as file:
+         template = file.read ()
+
+      rack_path = ''
+      if os.path.exists ('/Applications/VCV Rack 2 Pro.app/Contents/MacOS/Rack'):
+         rack_path = '/Applications/VCV Rack 2 Pro.app'
+      elif os.path.exists ('/Applications/VCV Rack 2 Free.app/Contents/MacOS/Rack'):
+         rack_path = '/Applications/VCV Rack 2 Free.app'
+      else:
+         print ('\033[33mwarning:\033[0m can\'t find VCV Rack executable')
+
+      identifier = ''.join (random.choice ('0123456789ABCDEF') for n in range (24))
+
+      template = template.replace ('%module.name%', module.name)
+      template = template.replace ('%identifier%', identifier)
+      template = template.replace ('%rack.path%', rack_path)
+
+      with open (path_xcscheme, 'w', encoding='utf-8') as file:
+         file.write (template)
