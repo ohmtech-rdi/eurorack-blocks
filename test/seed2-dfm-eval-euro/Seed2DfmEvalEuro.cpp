393c27d900cbdfc1e5e440d593d794e78cd57277
rdi <raphael.dinge@ohmforce.com>
2023-09-18 22:19:10 +0200

    fixup! Add tests for seed2 dfm eval euro board

--- test/seed2-dfm-eval-euro/Seed2DfmEvalEuro.cpp
+++ test/seed2-dfm-eval-euro/Seed2DfmEvalEuro.cpp
@@ -23,14 +23,59 @@ Name : process
 
 void  Seed2DfmEvalEuro::process ()
 {
+   const auto to_freq = [](float norm){
+      return 20.f * std::pow (500.f, std::abs (norm));
+   };
+
+   const auto to_shape = [](erb::SwitchPosition pos, float x){
+      switch (pos)
+      {
+      case erb::SwitchPosition::First:
+         return x;
+      case erb::SwitchPosition::Center:
+         return x * x * x;
+      case erb::SwitchPosition::Last:
+         return x > 0 ? 1.f : 0.f;
+      }
+   };
+
+   osc1.set (to_freq (std::clamp (ui.osc1_pot + ui.osc1_cv, 0.f, 1.f)));
+   osc2.set (to_freq (std::clamp (ui.osc2_pot + ui.osc2_cv, 0.f, 1.f)));
+   osc3.set (to_freq (std::clamp (ui.osc3_pot + ui.osc3_cv, 0.f, 1.f)));
+   osc4.set (to_freq (std::clamp (float (ui.osc4_pot), 0.f, 1.f)));
+
    for (size_t i = 0 ; i < erb_BUFFER_SIZE ; ++i)
    {
-      ui.audio_out1 [i]
-         = (ui.audio_in1 [i] + osc1 ())
+      ui.osc1_out [i]
+         = (ui.osc1_in [i] + osc1 ())
          * ((ui.osc1_but.held () || ui.osc1_gate) ? 1.f : 0.f);
 
-      ui.audio_out2 [i] = (ui.audio_in2 [i] + osc2 ());
-      ui.audio_out3 [i] = (ui.audio_in3 [i] + osc3 ());
-      ui.audio_out4 [i] = (ui.audio_in4 [i] + osc4 ());
+      ui.osc2_out [i] = ui.osc2_in [i] + osc2 ();
+      ui.osc3_out [i] = ui.osc3_in [i] + osc3 ();
+      ui.osc4_out [i] = to_shape (ui.osc4_shape, ui.osc4_in [i] + osc4 ());
+   }
+
+   ui.osc1_cv_out = ui.osc1_cv;
+   ui.osc2_cv_out = ui.osc2_cv;
+
+   pic = (pic + int (ui.enc) + 4) % 4;
+}
+
+
+
+/*
+==============================================================================
+Name : idle
+==============================================================================
+*/
+
+void  Seed2DfmEvalEuro::idle ()
+{
+   switch (pic)
+   {
+   case 0: ui.screen = Seed2DfmEvalEuroData::pic0; break;
+   case 1: ui.screen = Seed2DfmEvalEuroData::pic1; break;
+   case 2: ui.screen = Seed2DfmEvalEuroData::pic2; break;
+   case 3: ui.screen = Seed2DfmEvalEuroData::pic3; break;
    }
 }
