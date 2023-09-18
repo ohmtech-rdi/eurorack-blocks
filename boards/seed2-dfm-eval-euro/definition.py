0a7c98ca3c9345a6dde32881afaa69a526a65af3
rdi <raphael.dinge@ohmforce.com>
2023-09-18 14:24:20 +0200

    fixup! Add seed2 dfm eval euro board

---
+++ boards/seed2-dfm-eval-euro/definition.py
@@ -0,0 +1,116 @@
+##############################################################################
+#
+#     definition.py
+#     Copyright (c) 2020 Raphael DINGE
+#
+#Tab=3########################################################################
+
+
+
+{
+   'class': 'erb::BoardDaisyMicropatch',
+   'include': 'BoardDaisyMicropatch.h',
+   'pins': {
+      'CI1': {
+         'accept': ['CvIn'],
+         'bind': 'adc(0)',
+      },
+      'CI2': {
+         'accept': ['CvIn'],
+         'bind': 'adc(1)',
+      },
+      'CI3': {
+         'accept': ['CvIn'],
+         'bind': 'adc(2)',
+      },
+
+      'CO1': {
+         'accept': ['CvOut'],
+         'bind': 'dac(0)',
+      },
+
+      'CO2': {
+         'accept': ['CvOut'],
+         'bind': 'dac(1)',
+      },
+
+      'GI1': {
+         'accept': ['GateIn'],
+         'bind': 'gpi(0)',
+      },
+
+      'P1': {
+         'accept': ['Pot'],
+         'bind': 'adc(4)',
+      },
+      'P2': {
+         'accept': ['Pot'],
+         'bind': 'adc(5)',
+      },
+      'P3': {
+         'accept': ['Pot'],
+         'bind': 'adc(6)',
+      },
+      'P4': {
+         'accept': ['Pot'],
+         'bind': 'adc(7)',
+      },
+
+      'B1': {
+         'accept': ['Button'],
+         'bind': 'gpi(2)',
+      },
+
+      'B2': {
+         'accept': ['Switch'],
+         'bind': 'gpi(3)',
+      },
+      'B3': {
+         'accept': ['Switch'],
+         'bind': 'gpi(4)',
+      },
+
+      'B4': {
+         'accept': ['Encoder'],
+         'bind': 'gpi(5)',
+      },
+      'B5': {
+         'accept': ['Encoder'],
+         'bind': 'gpi(6)',
+      },
+
+      'AI1': {
+         'accept': ['AudioIn'],
+         'bind': 'audioin(0)',
+      },
+      'AI2': {
+         'accept': ['AudioIn'],
+         'bind': 'audioin(1)',
+      },
+      'AI3': {
+         'accept': ['AudioIn'],
+         'bind': 'audioin(2)',
+      },
+      'AI4': {
+         'accept': ['AudioIn'],
+         'bind': 'audioin(3)',
+      },
+
+      'AO1': {
+         'accept': ['AudioOut'],
+         'bind': 'audioout(0)',
+      },
+      'AO2': {
+         'accept': ['AudioOut'],
+         'bind': 'audioout(1)',
+      },
+      'AO3': {
+         'accept': ['AudioOut'],
+         'bind': 'audioout(2)',
+      },
+      'AO4': {
+         'accept': ['AudioOut'],
+         'bind': 'audioout(3)',
+      },
+   }
+}
