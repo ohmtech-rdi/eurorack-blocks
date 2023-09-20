5303057794c13f6eac9790fe8b586e4ff63190c4
rdi <raphael.dinge@ohmforce.com>
2023-09-20 15:02:25 +0200

    Add seed2 dfm eval euro board

---
+++ boards/seed2_dfm_eval_euro/BoardSeed2DfmEvalEuro.h
@@ -0,0 +1,26 @@
+/*****************************************************************************
+
+      BoardSeed2DfmEvalEuro.h
+      Copyright (c) 2020 Raphael DINGE
+
+*Tab=3***********************************************************************/
+
+
+
+#pragma once
+
+
+
+/*\\\ INCLUDE FILES \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+#if defined (erb_TARGET_DAISY)
+   #include "firmware/BoardSeed2DfmEvalEuro.h"
+
+#elif defined (erb_TARGET_VCV_RACK)
+   #include "simulator/BoardSeed2DfmEvalEuro.h"
+
+#endif
+
+
+
+/*\\\ EOF \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
---
+++ boards/seed2_dfm_eval_euro/definition.py
@@ -0,0 +1,121 @@
+##############################################################################
+#
+#     definition.py
+#     Copyright (c) 2020 Raphael DINGE
+#
+#Tab=3########################################################################
+
+
+
+{
+   'class': 'erb::BoardSeed2DfmEvalEuro',
+   'include': 'BoardSeed2DfmEvalEuro.h',
+   'pins': {
+      'CI1': {
+         'accept': ['CvIn'],
+         'bind': 'adc(0)',
+      },
+      'CI2': {
+         'accept': ['CvIn'],
+         'bind': 'adc(1)',
+      },
+      'CI3': {
+         'accept': ['CvIn'],
+         'bind': 'adc(2)',
+      },
+
+      'CO1': {
+         'accept': ['CvOut'],
+         'bind': 'dac(0)',
+      },
+
+      'CO2': {
+         'accept': ['CvOut'],
+         'bind': 'dac(1)',
+      },
+
+      'GI1': {
+         'accept': ['GateIn'],
+         'bind': 'gpi(0)',
+      },
+
+      'P1': {
+         'accept': ['Pot'],
+         'bind': 'adc(3)',
+      },
+      'P2': {
+         'accept': ['Pot'],
+         'bind': 'adc(4)',
+      },
+      'P3': {
+         'accept': ['Pot'],
+         'bind': 'adc(5)',
+      },
+      'P4': {
+         'accept': ['Pot'],
+         'bind': 'adc(6)',
+      },
+
+      'B1': {
+         'accept': ['Button'],
+         'bind': 'gpi(1)',
+      },
+
+      'B2': {
+         'accept': ['Switch'],
+         'bind': 'gpi(2)',
+      },
+      'B3': {
+         'accept': ['Switch'],
+         'bind': 'gpi(3)',
+      },
+
+      'B4': {
+         'accept': ['Encoder'],
+         'bind': 'gpi(4)',
+      },
+      'B5': {
+         'accept': ['Encoder'],
+         'bind': 'gpi(5)',
+      },
+
+      'AI1': {
+         'accept': ['AudioIn'],
+         'bind': 'audioin(0)',
+      },
+      'AI2': {
+         'accept': ['AudioIn'],
+         'bind': 'audioin(1)',
+      },
+      'AI3': {
+         'accept': ['AudioIn'],
+         'bind': 'audioin(2)',
+      },
+      'AI4': {
+         'accept': ['AudioIn'],
+         'bind': 'audioin(3)',
+      },
+
+      'AO1': {
+         'accept': ['AudioOut'],
+         'bind': 'audioout(0)',
+      },
+      'AO2': {
+         'accept': ['AudioOut'],
+         'bind': 'audioout(1)',
+      },
+      'AO3': {
+         'accept': ['AudioOut'],
+         'bind': 'audioout(2)',
+      },
+      'AO4': {
+         'accept': ['AudioOut'],
+         'bind': 'audioout(3)',
+      },
+
+      'OLED1': {
+         'accept': ['Display'],
+         'bind': 'oled()',
+      },
+   }
+}
