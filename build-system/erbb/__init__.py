beefe7cfe39b7ca0167c8cee1dd07d5c91a66438
rdi <raphael.dinge@ohmforce.com>
2023-12-04 21:01:43 +0100

    Fix bootloader version in erbb install

--- build-system/erbb/__init__.py
+++ build-system/erbb/__init__.py
@@ -694,10 +694,10 @@ Name : deploy_bootloader
 
 def deploy_bootloader ():
    libdaisy_bootloader_bin = os.path.join (
-      PATH_ROOT, 'submodules', 'libDaisy', 'core', 'dsy_bootloader_v4.bin'
+      PATH_ROOT, 'submodules', 'libDaisy', 'core', 'dsy_bootloader_v5_4.bin'
    )
 
-   deploy_dfu_util ('dsy_bootloader_v4', 'flash', libdaisy_bootloader_bin)
+   deploy_dfu_util ('dsy_bootloader_v5_4', 'flash', libdaisy_bootloader_bin)
 
 
 
